1b18710364db622e92a2ffebd997e318b8c9288f
nsakhrao <sakhraoui.nassim@gmail.com>
2018-04-03 17:47:34 -0400

    google map + fond

--- index.php
+++ index.php
@@ -24,9 +24,6 @@
         </div>
         <div class="menu-mobile">
             <div class="menu-btn" id="menu-hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
             </div>
             <nav>
                 <ul class="row-noshow">
@@ -57,17 +54,9 @@
     </div>
 </header>
 <main>
-    <div style="padding-left:16px">
-        <h2>Bien venu chez MelGood</h2>
-        <p>menu responsif.</p>
-        <div id="map">
+            <h2>Nos distributeurs :</h2>
             <!-- google map -->
-            <span>Nos distributeurs :</span><br>
-
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.30255662757!2d-73.6428947850151!3d45.54423883612114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc918e0c061b07f%3A0x647a6b6d7cb681a7!2sISI%2C+l&#39;Institut+sup%C3%A9rieur+d&#39;informatique!5e0!3m2!1sfr!2sca!4v1513800444286" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-            <h1>Contact Us</h1>
-        </div>
-    </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.885942531424!2d-73.59870968459427!3d45.53250073690465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc919645b41ee7f%3A0x92b2f188c27b5b15!2s760+Boulevard+Rosemont%2C+Montr%C3%A9al%2C+QC+H2S+3R2!5e0!3m2!1sfr!2sca!4v1522769760022" max-width=100% height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 </main>
 <footer>
     <footer>
