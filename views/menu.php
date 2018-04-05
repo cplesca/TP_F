6a85701251694cf2ee4a8f095f18eb60c6c98442
nsakhrao <sakhraoui.nassim@gmail.com>
2018-04-05 12:11:22 -0400

    correction merge

--- views/menu.php
+++ views/menu.php
@@ -1,15 +1,11 @@
 <?php
 require_once(dirname(__FILE__) . '/../defines.php');
-<<<<<<< HEAD
 require_once 'functions/tr.php';
 
 ?>
 <div class = "lang">
     <a href = "index.php?lang=<?= $lang==='fr'? 'en' : 'fr' ?> "><?= $lang==='fr'? 'en' : 'fr' ?></a>
 </div>
-=======
-?>
->>>>>>> b39e50724a7552c3ab58b55f5a56777a66e389d5
 <div id="header">
     <div class="col-m-2">
         <img src="images/logo.png" alt="logo">
@@ -17,32 +13,20 @@ require_once 'functions/tr.php';
     <div class="menu-mobile">
         <div class="menu-btn" id="menu-hamburger">
             <span></span>
-<<<<<<< HEAD
             <span></span>.
 
-=======
-            <span></span>
->>>>>>> b39e50724a7552c3ab58b55f5a56777a66e389d5
             <span></span>
         </div>
         <nav>
             <ul class="row-noshow">
                 <li class="col-4 col-m-3">
                     <div id="home">
-<<<<<<< HEAD
                         <a href="index.php"><?= tr("Acceuil") ?></a>
-=======
-                        <a href="index.php">Acceuil</a>
->>>>>>> b39e50724a7552c3ab58b55f5a56777a66e389d5
                     </div>
                 </li>
                 <li class="col-4 col-m-3">
                     <div id="service">
-<<<<<<< HEAD
-                        <a href="commande.php"><?= tr("Commande") ?></a>
-=======
-                        <a href="commande.php">Commander</a>
->>>>>>> b39e50724a7552c3ab58b55f5a56777a66e389d5
+                        <a href="comande.php"><?= tr("Commande") ?></a>
                     </div>
                 </li>
                 <li class="col-4 col-m-3">
