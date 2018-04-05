1d23ce88a78c7a15b3a6358b69374eea9b742acf
Unknown <sakhraoui.nassim@gmail.com>
2018-04-05 11:18:46 -0400

    Ajout page top et bottom

---
+++ views/page_top.php
@@ -0,0 +1,24 @@
+<?php
+require_once (dirname(__FILE__) . '/../defines.php');
+?>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>MelGood</title>
+    <meta name="MelGood" content="nassim/camile/corina" >
+    <link rel="stylesheet" href="style/main.css">
+    <script src="script/main.js"></script>
+    <script src="script/jquery-3.2.1.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header>
+    <?php require_once 'menu.php'; ?>
+</header>
+
+
