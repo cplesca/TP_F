28988488085f9357e29ca884b0baa8eedd4b355a
camgha <camgha@outlook.com>
2018-04-11 10:05:53 -0400

    page top retirer require once menu.php bouton en/fr rajouter sur page index enlever le css du bouton et mis dans .css rajouter image logo alléger le css

--- views/page_bottom.php
+++ views/page_bottom.php
@@ -37,7 +37,7 @@
             </div>
             <!-- end RESEAUX SOCIAUX -->
         </div>
-        <div id="map">
+        <div id="map" class="col-4">
             <!-- google map -->
             <span><?= tr("Nos distributeurs") ?>:</span><br>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.886129276202!2d-73.59870968444022!3d45.53249697910183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc919645b41ee7f%3A0x92b2f188c27b5b15!2s760+Boulevard+Rosemont%2C+Montr%C3%A9al%2C+QC+H2S+3R2!5e0!3m2!1sfr!2sca!4v1522769611991" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
