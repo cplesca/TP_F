ded5b57d599dab807e96991af372de034da082d0
cplesca <korywa@hotmail.com>
2018-04-12 10:36:15 -0400

    validasion de commande

---
+++ agenda.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 11 avr. 2018 à 14:03
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `agenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commende`
+--
+
+DROP TABLE IF EXISTS `commende`;
+CREATE TABLE IF NOT EXISTS `commende` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quantity` char(10) NOT NULL,
+  `full_name` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `phone` char(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `zipp_code` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
